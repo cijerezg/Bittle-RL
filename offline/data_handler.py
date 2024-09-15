02757581eaed623e75462abe32b140118bbcdbaf
cijerezg <35930061+cijerezg@users.noreply.github.com>
2024-09-15 18:37:08 -0500

    File to handle saving and loading data

---
+++ offline/data_handler.py
@@ -0,0 +1,18 @@
+import torch
+import h5py
+
+# This class dynamically saved data, and is able to load all the data.
+# Data will be stored in folders with the date
+
+class OfflineData():
+    def __init__(self, path):
+        self.path = path
+
+    def create_session(self):
+        
+    def store_episode(self):
+        
+    def load_data(self):
+
+    def save_data(self):
+        
