761872202f02a7709e869b2c50749840d80ed401
cijerezg <35930061+cijerezg@users.noreply.github.com>
2024-09-17 17:50:33 -0500

    Create client

--- client.py
+++ client.py
@@ -1,1 +1,33 @@
-# THis goes on the pi. Sends data from camera and sensors. and runs policy.
+import socket
+from run_policy import Robot
+import pickle
+import numpy as np
+
+host_ip = '10.56.136.219'
+port = 12345
+
+def main(policy):
+    bittle = Robot(policy)
+    action = np.zeros(16)
+    bittle.execute_action(action)
+    
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host_ip, port))
+    
+    image = bittle.capture_image()
+    dist = bittle.compute_distance()
+    action = policy(action, image, dist)
+
+    bittle.execute_action(action)
+    
+    image = image.tostring()
+    dist = dist.tostring()
+    action = action.tostring()
+
+    client_socket.sendall(image)
+    client_socket.sendall(dist)
+    client_socket.sendall(action)
+    
+    
+if __name__ == "__main__":
+    main(None)
