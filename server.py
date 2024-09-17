8adcca958fcb0e5100de60235bc126e36158056d
cijerezg <35930061+cijerezg@users.noreply.github.com>
2024-09-17 17:56:52 -0500

    Socket communication server

--- server.py
+++ server.py
@@ -1,1 +1,26 @@
-# This is the data received by the PC. Sends back modified weights ocassionally.
+import socket
+
+def main():
+    server_address = ('', 12345)
+  
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(server_address)
+    server_socket.listen(1)
+
+    conn, addr = server_socket.accept()
+
+    with conn:
+        data = conn.recv(2048)
+        if data:
+            val = data.decode()
+        else:
+            print('No data')
+            val = None
+    
+    return val
+
+if __name__ = "__main__":
+    main()
+    
+  
+  
