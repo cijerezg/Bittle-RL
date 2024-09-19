de7b7006c86b14c71bd681eaff4afd1b2baf84f3
cijerezg <35930061+cijerezg@users.noreply.github.com>
2024-09-19 14:14:02 -0500

    Add priority queue to store frames

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,7 @@
 import torch
 import copy
+from collections import deque
+
 
 def get_params(model):
     params = {}
@@ -7,3 +9,13 @@ def get_params(model):
         params[name] = nn.Parameter(param)
     return copy.deepcopy(params)
         
+
+class LimitedQueue:
+    def __init__(self, max_size=4):
+        self.queue = deque(maxlen=max_size)
+
+    def add(self, item):
+        self.queue.append(item)
+
+    def get_items(self):
+        return list(self.queue)
