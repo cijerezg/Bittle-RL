b61f26bc67b410651280875b399e75adf3aafa61
cijerezg <35930061+cijerezg@users.noreply.github.com>
2024-09-18 22:13:12 -0500

    Create agent to run policy. Need to update robot action.

--- rl/agent.py
+++ rl/agent.py
@@ -1,1 +1,17 @@
-# Computes losses and training and all that stuff.
+import torch
+from models.architectures import Critic, Policy
+import copy
+from torch.func import functional_call
+
+
+class Actor():
+    def __init__(self, im_size, action_range):
+        self.policy = Policy(im_size, action_range)
+        self.max_angle = 125
+
+    def policy(self, params, x):
+        sample, density, mu, std = functional_call(self.policy, params, x)
+        return sample, density, mu, std
+
+    def robot_action(self, sample):
+        # Check with robot
